d20833eee0d2c9ae50a11fd017ea60c4a73ccdbe
Martin Devera <devik@eaxlabs.cz>
2018-07-26 12:12:47 +0200

    Add HT32F5xx flash driver

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -38,6 +38,7 @@ extern struct flash_driver em357_flash;
 extern struct flash_driver faux_flash;
 extern struct flash_driver fm3_flash;
 extern struct flash_driver fm4_flash;
+extern struct flash_driver ht32f5xxx_flash;
 extern struct flash_driver jtagspi_flash;
 extern struct flash_driver kinetis_flash;
 extern struct flash_driver kinetis_ke_flash;
@@ -94,6 +95,7 @@ static struct flash_driver *flash_drivers[] = {
 	&faux_flash,
 	&fm3_flash,
 	&fm4_flash,
+	&ht32f5xxx_flash,
 	&jtagspi_flash,
 	&kinetis_flash,
 	&kinetis_ke_flash,
